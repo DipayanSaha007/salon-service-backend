1e8ca91fc4a2bd3274e0c86cad5551ffde3035ae
DipayanSaha007 <143510835+DipayanSaha007@users.noreply.github.com>
2024-11-15 18:41:35 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
-require('dotenv').config({ path: './dead.env' });
+require('dotenv').config();  // Remove path as Vercel injects the environment variables automatically
 
 const app = express();
 const PORT = process.env.PORT || 5000;
